ded4797535ed28b17552463eb37264863da03233
Kumail Abbas <smk.xaidi@gmail.com>
2022-02-04 23:27:39 +0500

    Added configuration module, aws-sdk, typeorm and mysql packages

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,28 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import configuration from './configuration';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      load: [configuration],
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('database.host'),
+        port: configService.get<number>('database.port'),
+        username: configService.get<string>('database.user'),
+        password: configService.get<string>('database.password'),
+        database: configService.get<string>('database.name'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: true,
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'aws-sdk';
 
 const PORT = process.env.PORT || 4000;
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const configService = app.get(ConfigService);
+
+  config.update({
+    accessKeyId: configService.get('aws.access_key_id'),
+    secretAccessKey: configService.get('aws.secret_access_key'),
+    region: configService.get('aws.region'),
+  });
+
   await app.listen(PORT);
 }
 bootstrap();
